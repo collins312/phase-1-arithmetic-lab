61a1358c954d1df981a2b51bdf3e1980c345ea8f
collins otieno <collins.ouma2@student.moringaschool.com>
2025-06-05 00:46:33 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+var num1 = 2;
+var num2 =31;
+var multiply = num1 * num2;
 // Write your code here
+var random = Math.floor(Math.random() * 100) + 1;
+var num3 = 10;
+var num4 = 6;
+var mod = num3 % num4;
+var max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
